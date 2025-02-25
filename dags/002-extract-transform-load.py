263b0b7a180430c9ab2bad8df21348e701e9e0e2
Laura Kaminskiy <itslaurakaminskiy@gmail.com>
2025-02-25 12:03:30 -0500

    updating: docstrings

--- dags/002-extract-transform-load.py
+++ dags/002-extract-transform-load.py
@@ -1,19 +1,35 @@
+from typing import Dict, Any
+
 from airflow import DAG
+from airflow.models import TaskInstance
 from airflow.operators.python import PythonOperator
 
 with DAG('lauras-extract-transform-load-DAG', description='A simple tutorial DAG', ):
-    def extract(**kwargs):
+    def extract(**kwargs) -> None:
         """
-        Extracts the sample data.
-        :param kwargs:
-        :return:
+        Extracts sample data.
+        Args:
+            **kwargs: Python callable arguments
+
+        Returns:
+            None
         """
-        ti = kwargs['ti']
+        kwargs: Dict[str, Any]
+        ti: TaskInstance = kwargs.get('ti')
         data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
         ti.xcom_push(key='order_data', value=data_string)
 
 
-    def transform(**kwargs):
+    def transform(**kwargs) -> None:
+        """
+        Transforms sample data.
+        Args:
+            **kwargs: Python callable arguments
+
+        Returns:
+            None
+        """
+        kwargs: Dict[str, Any]
         ti = kwargs['ti']
         order_data = ti.xcom_pull(key='order_data', task_ids='extract')
         total_amount = sum(order_data.values())
